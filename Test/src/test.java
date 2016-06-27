9270e2b69cc2178838246daaa45d5435f797cbbb
dzk792 <dzk792@DERUEWNCE35JQT3.eur.corp.gm.com>
2016-06-27 15:25:24 +0200

    Update

--- Test/src/test.java
+++ Test/src/test.java
@@ -2,14 +2,8 @@
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-<<<<<<< HEAD
-System.out.println("test");
-// erste Ausgabe dritter Versuch
-=======
-System.out.println("Jonatahan");
-// erste Ausgabe zweiter Versuch
->>>>>>> branch 'Test' of https://github.com/djoliboli/DHBW_MessageBroker.git
+
+System.out.println("Joni hat kein Plan wie das funktioniert");
 	}
 
 }
