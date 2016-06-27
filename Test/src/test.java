586f6760fc325d450e399380d785128d65cdc198
JZX8NT <JZX8NT@DERUEWNCE6RSKT3.eur.corp.gm.com>
2016-06-27 15:26:23 +0200

    asdf

--- Test/src/test.java
+++ Test/src/test.java
@@ -3,7 +3,7 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Testhosteronatahan");
+System.out.println("Testosteronatahan");
 // erste Ausgabe zweiter Versuch
 	}
 
